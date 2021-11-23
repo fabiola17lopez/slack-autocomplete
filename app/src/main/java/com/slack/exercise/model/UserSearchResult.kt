77206a5e242b1f1ae276c2e8edcaa0cf23ad1ae3
fabiola17lopez <fabiola17lopez@gmail.com>
2021-11-22 23:39:41 -0600

    Implement design's UI specs (#2)
    * Add styling for viewHolder
    
    * Load images

--- app/src/main/java/com/slack/exercise/model/UserSearchResult.kt
+++ app/src/main/java/com/slack/exercise/model/UserSearchResult.kt
@@ -3,4 +3,8 @@ package com.slack.exercise.model
 /**
  * Models users returned by the API.
  */
-data class UserSearchResult(val username: String)
+data class UserSearchResult(
+    val username: String,
+    val displayName: String,
+    val avatarUrl: String
+)
--- app/src/main/java/com/slack/exercise/ui/usersearch/UserSearchAdapter.kt
+++ app/src/main/java/com/slack/exercise/ui/usersearch/UserSearchAdapter.kt
@@ -3,11 +3,16 @@ package com.slack.exercise.ui.usersearch
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.slack.exercise.R
 import com.slack.exercise.model.UserSearchResult
-import kotlinx.android.synthetic.main.item_user_search.view.username
+import kotlinx.android.synthetic.main.item_user_search.view.*
+
+private const val ROUNDED_CORNER_RADIUS = 8
 
 /**
  * Adapter for the list of [UserSearchResult].
@@ -30,10 +35,20 @@ class UserSearchAdapter : RecyclerView.Adapter<UserSearchAdapter.UserSearchViewH
   }
 
   override fun onBindViewHolder(holder: UserSearchViewHolder, position: Int) {
-    holder.username.text = userSearchResults[position].username
+    val result = userSearchResults[position]
+    holder.username.text = result.username
+    holder.displayName.text = result.displayName
+
+    Glide.with(holder.itemView.context)
+      .load(result.avatarUrl)
+      .fitCenter()
+      .transform(RoundedCorners(ROUNDED_CORNER_RADIUS))
+      .into(holder.avatar)
   }
 
   class UserSearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val username: TextView = itemView.username
+    val displayName: TextView = itemView.displayName
+    val avatar: ImageView = itemView.avatar
   }
 }
